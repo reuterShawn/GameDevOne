de2448cded6ed02f58ea8f8195f07e3b1fe44008
Johannes Martinez <mart4695@morris.umn.edu>
2022-02-02 21:09:10 -0600

    Remove main screen buttons and text when clicking on credits button

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -9,14 +9,21 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private Button openCreditsButton;
     [SerializeField] private Button closeCreditsButton;
     [SerializeField] private GameObject creditsUI;
+    [SerializeField] private GameObject mainScreen;
     
     // Start is called before the first frame update
     void Start()
     {
         creditsUI.SetActive(false);
+        mainScreen.SetActive(true);
+
         startButton.onClick.AddListener(() => LoadingScreen.LoadScene("GameScene1"));
-        openCreditsButton.onClick.AddListener(() => {creditsUI.SetActive(true);});
-        closeCreditsButton.onClick.AddListener(() => {creditsUI.SetActive(false);});
+
+        openCreditsButton.onClick.AddListener(() => {creditsUI.SetActive(true); 
+        mainScreen.SetActive(false);});
+
+        closeCreditsButton.onClick.AddListener(() => {creditsUI.SetActive(false);
+        mainScreen.SetActive(true);});
     }
 
     // Update is called once per frame
