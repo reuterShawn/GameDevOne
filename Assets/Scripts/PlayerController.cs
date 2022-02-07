c41b3d1fee3f10b1ee50c64916aba02076b9cd4f
Shawn Reuter <reute078@morris.umn.edu>
2022-02-07 12:43:00 -0600

    Add player ability to move left and right

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D mainRigidBody;
+    [SerializeField] private SpriteRenderer mainSpriteRenderer;
+
+    [SerializeField] private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            mainRigidBody.AddForce(new Vector2(-moveSpeed * Time.deltaTime, 0));
+            mainSpriteRenderer.flipX = true;
+        } else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            mainRigidBody.AddForce(new Vector2(moveSpeed * Time.deltaTime, 0));
+            mainSpriteRenderer.flipX = false;
+        }
+
+    }
+}
