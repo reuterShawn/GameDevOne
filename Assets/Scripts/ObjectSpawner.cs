b4879c4c41c05e01437955b8b01afaa7396bc178
Shawn Reuter <reute078@morris.umn.edu>
2022-02-13 15:18:55 -0600

    Add ObjectCounter text to scene

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] private Transform spawnLocation;
     [SerializeField] private SpawnObject objectPrefab;
+    [SerializeField] private TMP_Text objectCountText;
+    private int objectCount = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +24,9 @@ public class ObjectSpawner : MonoBehaviour
             newObject.SetColor(Random.ColorHSV(0, 1, 0.75f, 1, 0.5f, 1, 1, 1));
             newObject.transform.position = spawnLocation.position;
             newObject.transform.rotation = Random.rotation;
+
+            objectCount++;
+            objectCountText.text = "Object Count: " + objectCount;
         }
         
     }
