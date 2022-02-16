b8d5cb25b69075321f208ab75042d3436299a300
Shawn Reuter <reute078@morris.umn.edu>
2022-02-16 12:52:17 -0600

    Add field and if statement to PongPaddle script

--- Assets/Scripts/Pong/PongPaddle.cs
+++ Assets/Scripts/Pong/PongPaddle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PongPaddle : MonoBehaviour
 {
+    [SerializeField] private KeyCode upKey;
+    [SerializeField] private KeyCode downKey;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PongPaddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        } else if (Input.GetKey(KeyCode.S)) {
+
+        }
     }
 }
