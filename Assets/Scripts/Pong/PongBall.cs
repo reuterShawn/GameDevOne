2869fac2e563528e18daaead8666431a06e979b2
Shawn Reuter <reute078@morris.umn.edu>
2022-02-18 22:29:05 -0600

    Add PongBall code and tweaking paddle speed

--- Assets/Scripts/Pong/PongBall.cs
+++ Assets/Scripts/Pong/PongBall.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class PongBall : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D mainRigidbody;
+    [SerializeField] private float startSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        StartCoroutine(Restart());
+        IEnumerator Restart()
+        {
+            mainRigidbody.position = Vector2.zero;
+            mainRigidbody.velocity = Vector2.zero;
 
+            Vector2 newVelocity = new Vector2(Random.Range(0f, 5f), Random.Range(0f, 1f));
+            mainRigidbody.velocity = newVelocity.normalized * startSpeed;
+            yield return null;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
