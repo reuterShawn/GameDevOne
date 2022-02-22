414f918c50ce7884667244b778d4bba964ab60b9
Johannes Martinez <mart4695@morris.umn.edu>
2022-02-21 20:13:32 -0600

    Detect hit goal posts

--- Assets/Scripts/Pong/LeftPongGoal.cs
+++ Assets/Scripts/Pong/LeftPongGoal.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class PongGoal : MonoBehaviour
+public class LeftPongGoal: MonoBehaviour
 {
 
-    public event Action onScore;
+    public static event Action onScore;
 
-    [SerializeField] private Transform p1Goal;
-    [SerializeField] private Transform p2Goal;
     // [SerializeField] private TMP_Text goalNumberText;
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Ball")) {
             onScore?.Invoke();
             Debug.Log("Something is happening");
-
         }
     }
 
--- Assets/Scripts/Pong/PongManager.cs
+++ Assets/Scripts/Pong/PongManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 
 public class PongManager : MonoBehaviour
 {  
-    [SerializeField] private float HandleP1Score;
-    [SerializeField] private float HandleP2Score;
 
    private void Awake() {
-    //    p1Goal.onScore += HandleP2Score;
-    //    p2Goal.onScore += HandleP1Score;
-    //    PongBall.Restart();
+       LeftPongGoal.onScore += HandleP1Score;
+       RightPongGoal.onScore += HandleP2Score;
+       // PongBall.Restart();
+   }
+
+   void HandleP2Score(){
+       Debug.Log("Right goal post hit");
+   }
+
+    void HandleP1Score() {
+       Debug.Log("Left goal post hit");
    }
 }
