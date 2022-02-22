039f10c9a033eb687195f7af91ea2f6212c01c3a
Shawn Reuter <reute078@morris.umn.edu>
2022-02-21 20:46:04 -0600

    Add score text and color them

--- Assets/Scripts/Pong/PongManager.cs
+++ Assets/Scripts/Pong/PongManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 public class PongManager : MonoBehaviour
 {  
+
+    [SerializeField] private TMP_Text rightText;
+    [SerializeField] private TMP_Text leftText;
+    private int rightCount = 0;
+    private int leftCount = 0;
 
    private void Awake() {
        LeftPongGoal.onScore += HandleP1Score;
@@ -14,9 +20,13 @@ public class PongManager : MonoBehaviour
 
    void HandleP2Score(){
        Debug.Log("Right goal post hit");
+       rightCount++;
+       rightText.text = "" + rightCount;
    }
 
     void HandleP1Score() {
        Debug.Log("Left goal post hit");
+       leftCount++;
+       leftText.text = "" + leftCount;
    }
 }
