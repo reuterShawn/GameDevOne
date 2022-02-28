4a5bc4fa2b5b13b536045a954403bd37b440bd9b
Shawn Reuter <reute078@morris.umn.edu>
2022-02-28 13:14:11 -0600

    Create script FPSEnemySpawner
    Currently spawning wolves is not working

--- Assets/Lab6Assets/scripts/FPSEnemy.cs
+++ Assets/Lab6Assets/scripts/FPSEnemy.cs
@@ -5,44 +5,26 @@ using UnityEngine;
 public class FPSEnemy : MonoBehaviour
 {
     [SerializeField] private Transform mainTransform;
-    [SerializeField] private Transform spawnLocation;
-    [SerializeField] private GameObject[] enemies;
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float moveTime;
-    [SerializeField] private float lastSpawnTime;
-    [SerializeField] private float secondsPerSpawn;
-
- void Start()
+    void Update()
     {
-        
+        Vector3 playerPos = FPSPlayer.instance.transform.position;
+        mainTransform.LookAt(playerPos);
+        Vector3 currentRotation = mainTransform.rotation.eulerAngles;
+        currentRotation.x = 0;
+        mainTransform.eulerAngles = currentRotation;
+        Vector3 directionToPlayer = (playerPos - mainTransform.position).normalized;
+        mainTransform.position += (directionToPlayer * moveSpeed * Time.deltaTime).SetY(0);
     }
-  
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Bullet")) {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
             Destroy(gameObject);
             Destroy(other.gameObject);
             FPSPlayer.instance.HandleEnemyDefeat();
             Debug.Log("Enemy hit!");
         }
-    }
-    private void Update()
-    {
-    Vector3 playerPos = FPSPlayer.instance.transform.position;
-        mainTransform.LookAt(playerPos);
-        Vector3 currentRotation = mainTransform.rotation.eulerAngles;
-        secondsPerSpawn -= (0.05f * Time.deltaTime);
-        if (Time.time - lastSpawnTime >= secondsPerSpawn && FPSPlayer.instance.ShouldSpawn(spawnLocation.position))
-        {
-            lastSpawnTime = Time.time;
-            Spawn();
-        }
-    }
-  
-
-  
-    private void Spawn() {
-        GameObject enemyPrefab = enemies[Random.Range(0, enemies.Length)];
-        GameObject newEnemy = Instantiate(enemyPrefab);
-        newEnemy.transform.position = spawnLocation.position;
     }
 }
