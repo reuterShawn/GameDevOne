8b821cb81612847e330979b58d3c8340035f3a9f
Shawn Reuter <reute078@morris.umn.edu>
2022-02-28 12:52:33 -0600

    Fix errors with Enemy script

--- Assets/Lab6Assets/scripts/FPSEnemy.cs
+++ Assets/Lab6Assets/scripts/FPSEnemy.cs
@@ -16,11 +16,6 @@ public class FPSEnemy : MonoBehaviour
     {
         
     }
-     void Update()
-    {
-        Vector3 playerPos = FPSPlayer.instance.transform.position;
-        mainTransform.LookAt(playerPos);
-    }
   
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Bullet")) {
@@ -30,29 +25,21 @@ public class FPSEnemy : MonoBehaviour
             Debug.Log("Enemy hit!");
         }
     }
-
+    private void Update()
+    {
+    Vector3 playerPos = FPSPlayer.instance.transform.position;
+        mainTransform.LookAt(playerPos);
         Vector3 currentRotation = mainTransform.rotation.eulerAngles;
-        currentRotation.x = 0;
-        mainTransform.eulerAngles = currentRotation;
-        Vector3 directionToPlayer = (playerPos - mainTransform.position).normalized;
-        mainTransform.position += (directionToPlayer * moveSpeed * Time.deltaTime).SetY(0);
         secondsPerSpawn -= (0.05f * Time.deltaTime);
-        if(Time.time - lastSpawnTime >= secondsPerSpawn && FPSPlayer.instance.ShouldSpawn(spawnLocation.position)) {
+        if (Time.time - lastSpawnTime >= secondsPerSpawn && FPSPlayer.instance.ShouldSpawn(spawnLocation.position))
+        {
             lastSpawnTime = Time.time;
             Spawn();
         }
     }
+  
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Bullet"))
-        {
-            Destroy(gameObject);
-            Destroy(other.gameObject);
-            FPSPlayer.instance.HandleEnemyDefeat();
-            Debug.Log("Enemy Killed HandleEnemyDefeat Called");
-        }
-    }
+  
     private void Spawn() {
         GameObject enemyPrefab = enemies[Random.Range(0, enemies.Length)];
         GameObject newEnemy = Instantiate(enemyPrefab);
